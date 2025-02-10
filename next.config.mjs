f270ccfaa27a497557a676bea6e69a099b45e428
Ewen Lukacik <newelukacik7360@gmail.com>
2025-02-10 14:23:56 +0100

    fix: put env

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    publicRuntimeConfig: {
+        basePath: process.env.BASE_PATH || '',
+    },
+    basePath: process.env.BASE_PATH || '',
+};
 
 export default nextConfig;
